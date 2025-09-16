85598104b894576caf144fcbc2030e77d28a67c4
Bernie Zhong <chu23008@byui.edu>
2025-09-16 22:26:00 +0800

    w03 learning activity 1/3 done(classification page)

---
+++ database/index.js
@@ -0,0 +1,39 @@
+// 根據目前的環境（開發 or 上線）來建立資料庫連線方式，並提供一個簡單好用的 .query() 函式讓其他檔案存取資料庫。
+
+const { Pool } = require("pg")
+require("dotenv").config()
+/* ***************
+ * Connection Pool
+ * SSL Object needed for local testing of app
+ * But will cause problems in production environment
+ * If - else will make determination which to use
+ * *************** */
+let pool
+if (process.env.NODE_ENV == "development") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+})
+
+// Added for troubleshooting queries
+// during development
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params)
+      console.log("executed query", { text })
+      return res
+    } catch (error) {
+      console.error("error in query", { text })
+      throw error
+    }
+  },
+}
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
+  module.exports = pool
+}
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+
 /* ***********************
 * View Engine and Template
 *************************/
@@ -26,9 +29,9 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 
 /* ***********************
